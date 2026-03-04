5aadec661999cc7c5784fd14c67106d213e49eaf
manueltrs <mjulianm29@gmail.com>
2026-03-04 00:22:08 -0500

    creacion de clases User,Agent,TaskItem. CARPETA DE MODELOS. Creacion de conexion con DB y carpeta

---
+++ SmartAgent/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartAgent.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public string Role { get; set; } = "User";
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Agent>? Agents { get; set; }
+    }
+}
