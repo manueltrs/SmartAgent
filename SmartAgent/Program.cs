70002023c625558008a78adf9216818c86c2a688
manueltrs <mjulianm29@gmail.com>
2026-03-11 13:45:50 -0500

    CORS permitir cualquier origen temporalmente

--- SmartAgent/Program.cs
+++ SmartAgent/Program.cs
@@ -11,10 +11,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins(
-                "http://localhost:5173",
-                "https://smart-agent-puce.vercel.app"
-              )
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
