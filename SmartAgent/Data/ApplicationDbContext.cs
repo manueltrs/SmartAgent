bb90d3c8e0fb15f42946912873f33d92328fe9ec
manueltrs <mjulianm29@gmail.com>
2026-03-11 13:05:50 -0500

    Restaurar archivos eliminados por merge

---
+++ SmartAgent/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartAgent.Models;
+
+namespace SmartAgent.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Agent> Agents { get; set; }
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
